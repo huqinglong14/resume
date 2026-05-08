66221dc620f6b4d576b6643c4e3bbfe6b9d99724
胡庆龙 <1367676275@qq.com>
2026-05-08 15:52:07 +0800

    路径多了一个/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const resolveHtml = (file: string) => decodeURIComponent(new URL(file, import.meta.url).pathname.slice(1));
+const resolveHtml = (file: string) => {
+  const pathname = new URL(file, import.meta.url).pathname;
+  return /^\/[A-Za-z]:\//.test(pathname) ? pathname.slice(1) : pathname;
+};
 
 export default defineConfig({
   plugins: [react()],
